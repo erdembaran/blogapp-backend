52596375581cd018d30bc9f4fe45dcd904c3b7c8
“Baran <109768322+erdembaran@users.noreply.github.com>
2024-03-22 20:51:48 +0300

    feat: add security and performance for the app

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 const db = require("./config/db");
 const userRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
 const errorHandler = require("./middleware/error");
+const helmet = require("helmet");
+const compression = require("compression");
+const app = express();
+
+app.use(helmet());
+app.use(compression());
 
 dotenv.config();
 
